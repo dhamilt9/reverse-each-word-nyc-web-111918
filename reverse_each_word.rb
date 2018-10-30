a78ebf07c12877f339138801a04b1c79f67bbd24
Daniel Hamilton <danhamilton16@gmail.com>
2018-10-30 20:35:35 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   returnstring=""
   array=string.split(" ")
-  array.collect{ |x| x.reverse }.join(" ")
+  return array.collect{ |x| x.reverse }.join(" ")
 end
 
 reverse_each_word("Hello my name is Dan")
